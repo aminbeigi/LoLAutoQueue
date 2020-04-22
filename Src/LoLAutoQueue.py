4bf8d9d1e1bf0f4a62fcf4fb427c134684ea59c2
Amin Ghasembeigi <amin.ghasembeigi@outlook.com>
2020-04-22 17:07:07 +1000

    Update LoLAutoQueue.py

--- Src/LoLAutoQueue.py
+++ Src/LoLAutoQueue.py
@@ -6,17 +6,19 @@ import win32api, win32con
 import os, time 
 
 """
-All coordinates assume a screen resoultion of 2560x1440, and 
-a league client resolution 1280x720.
-Directory needs to be added line 35 and 36.
+All click functions assume a screen resoultion of 2560x1440.
+Directory needs to be added line 13 and 14.
 """
+
+templates = ""
+capture = ""
 
 def get_coords():
 	x,y = win32api.GetCursorPos()
 	print(x,y)
 
 def test():
-	cv2.imshow("img", img)
+	cv2.imshow("image", capture_img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 	print(loc)
@@ -29,39 +31,46 @@ def click(x,y):
 	win32api.SetCursorPos((x,y))
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-	print("Clicking on ({}, {})".format(x, y))
+	print("Clicking on accept match button at ({}, {})".format(x, y))
 
-def find_matches():
-	accept_match_button = "INSERT DIRECTORY"
-	capture = "INSERT DIRECTORY"
-
+def auto_queue():
+	time.sleep(1)
 	screen_grab()
 
-	img = cv2.imread(capture)
-	grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	template = cv2.imread(accept_match_button, 0)
-	w, h = template.shape[::-1]
-	res = cv2.matchTemplate(grey_img, template, cv2.TM_CCOEFF_NORMED)
-	threshold = 0.9;
-	loc = np.where(res >= threshold)
+	# reading capture.png
+	capture_img = cv2.imread(capture)
+	capture_grey_img = cv2.cvtColor(capture_img, cv2.COLOR_BGR2GRAY)
 
-	for pt in zip(*loc[::-1]):
-		cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+	# loop through different button sizes
+	print("Attempting to match template...")
+	for i in range(4):
+		template = cv2.imread(templates.format(i), 0)
 
-	try:
-		x = pt[0] 
-		y = pt[1] -- 40 #adjust for openCV
-		click(x,y)
-		print("Successfully matched template... \n Accepting queue.")
-		exit()
-	except UnboundLocalError:
-		print("Couldn't locate and match template image...")
-		time.sleep(4)
-		pass   
+		# MATCHING TEMPLATES
+		w, h = template.shape[::-1]
+		res = cv2.matchTemplate(capture_grey_img, template, cv2.TM_CCOEFF_NORMED)
+		threshold = 0.9;
+		loc = np.where(res >= threshold)
+
+		# DRAWING RECTANGLES
+		for pt in zip(*loc[::-1]):
+			cv2.rectangle(capture_img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+		try:
+			x = pt[0] 
+			y = pt[1] -- 40 # adjust for openCV border
+			click(x,y)
+			cv2.imshow("image", capture_img)
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
+			print("Successfully matched template. \nAccepting queue.")
+			exit()
+		except UnboundLocalError:
+			print("Couldn't locate and match button{}.png...".format(i))
+			pass   
 
 def main():
 	while True:
-		find_matches() 
+		auto_queue() 
 
 if __name__ == '__main__':
 	main()
